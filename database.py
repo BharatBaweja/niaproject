42928fdafcbca18a43b34d9c3bd9e8456aa2f3ff
BharatBaweja <46478167+BharatBaweja@users.noreply.github.com>
2021-09-08 17:58:14 +0530

    v2

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ server = 'niaagro.database.windows.net'
 database = 'NiaAgro'
 username = 'niaagro'
 password = 'Agro@123456'   
-driver='{ODBC Driver 13 for SQL Server}'
+driver='{ODBC Driver 17 for SQL Server}'
 mysql = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
